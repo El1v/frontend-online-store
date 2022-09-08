9cf4090eb930e02eac5d7669e030612937d43c19
Elivelton <eliveltonmn@outlook.com>
2022-09-08 19:17:09 -0300

    agora vai

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,7 +1,7 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
 import { getProductsFromCategoryAndQuery, getCategories } from '../services/api';
-
+//
 class Home extends React.Component {
   state = {
     search: '',
