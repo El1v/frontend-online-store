2c8ee57b59e230ac17636f294393529fa659ddc5
Elivelton <eliveltonmn@outlook.com>
2022-09-08 19:12:18 -0300

    Grupo 13 - arruma lint 2

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -37,7 +37,6 @@ class Home extends React.Component {
   };
 
   render() {
-
     const { search, listSearchResults, buttonIsClicked, listOfCategories } = this.state;
     let searchProducts;
     if (listSearchResults.length > 0) {
