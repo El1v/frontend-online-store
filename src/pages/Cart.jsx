88e991e21b88ad29bbe17d0263242f20f7b568f9
Alison Pinheiro <alisonlivee@gmail.com>
2022-09-08 16:57:42 -0300

    Grupo 13 Req 03 Cria carrinho de compra

---
+++ src/pages/Cart.jsx
@@ -0,0 +1,22 @@
+import React from 'react';
+import { Link } from 'react-router-dom';
+// import PropTypes from 'prop-types';
+
+export default class Cart extends React.Component {
+  render() {
+    return (
+      <div>
+        <Link
+          to="/"
+        >
+          Voltar para pagina incial
+        </Link>
+        <div data-testid="shopping-cart-empty-message"> Seu carrinho está vazio!!!! </div>
+      </div>
+    );
+  }
+}
+
+Cart.propTypes = {
+  // cart: PropTypes.arrayOf(PropTypes.shape()).isRequired,
+};
